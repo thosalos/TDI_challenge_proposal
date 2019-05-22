42b11a18665b415617e36339ddb98215312c5c6a
Thomas Keller <bender@bender.local>
2019-05-21 23:13:23 -0700

    Update analysis with additional models

--- well_yield_analysis.R
+++ well_yield_analysis.R
@@ -13,6 +13,9 @@ library(rgdal)
 library(rspatial)
 library(dismo)
 library(gstat)
+library(spdep)
+library(spatialreg)
+library(elevatr)
 
 #wells <- read_csv("OSWCR.csv")
 
@@ -153,8 +156,7 @@ county_production <- ggplot(production_counts) +
 county_production
 
 
-
-# Local well production in Sonoma
+# Local well production in the North Bay
 ca_county <- subset(counties, region == "california")
 sonoma <- subset(ca_county, subregion == "sonoma")
 sonoma_wells <- subset(cali_wells, CountyName == "Sonoma")
@@ -172,7 +174,6 @@ sonoma_map <- ggmap(sonoma_map_data) +
   ggtitle("Domestic Well Yield in Sonoma County Varies with Geography")
 
 sonoma_map
-
 
 
 # classify well production (Low, Medium, High) using raw Long/Lat and other predictors with a random forest
@@ -193,13 +194,10 @@ valid_set <- cali_production[-train,]
 model <- randomForest(production ~ ., data = train_set, ntree = 500, mtry = 6, importance = TRUE)
 model
 
-
-
 # Predicting on train set
 pred_train <- predict(model, train_set, type = "class")
 # Checking classification accuracy
 table(pred_train, train_set$production)  
-
 
 
 # Predicting on Validation set
@@ -207,9 +205,6 @@ pred_valid <- predict(model, valid_set, type = "class")
 # Checking classification accuracy
 mean(pred_valid == valid_set$production)                    
 table(pred_valid,valid_set$production)
-
-
-
 
 importance(model)
 varImpPlot(model)
@@ -219,7 +214,7 @@ varImpPlot(model)
 # Develop a model that works better with location data
 well_coords <- dplyr::select(cali_features, DecimalLongitude, DecimalLatitude)
 well_sp <- SpatialPoints(well_coords, proj4string=CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
-well_sp <- SpatialPointsDataFrame(well_sp, cali_features)
+well_sp <- SpatialPointsDataFrame(well_sp, north_wells)
 cali_sp <- sp_data("counties")
 #define groups for mapping
 cuts <- c(0, 10, 20, 50, 75, Inf)
@@ -228,7 +223,6 @@ blues <- colorRampPalette(c('yellow', 'orange', 'blue', 'dark blue'))
 pols <- list("sp.polygons", cali_sp, fill = "lightgray")
 yield_distribution <- spplot(well_sp, "WellYield", cuts=cuts, col.regions=blues(5), sp.layout=pols, pch=20, cex=0.5, 
                              colorkey = TRUE, main = "Distribution of Well Yields (GPM)")
-
 
 
 # Voronoi set to define all regions between well data points and define by production
@@ -275,9 +269,6 @@ rmse
 mean(rmse)
 1 - (mean(rmse) / null)
 
-
-
-
 # Interpolate well production from the 5 nearest neighbors
 
 gs <- gstat(formula=WellYield~1, locations=well_ta, nmax=5, set=list(idp = 0))
@@ -310,7 +301,6 @@ idwr <- mask(idw, vr)
 plot(idwr, main = "Weight NN Interpolation of Well Production", axes = FALSE)
 
 
-
 # Weighted Nearest Neighbor Five-fold cross-validation
 
 rmse <- rep(NA, 5)
@@ -325,3 +315,104 @@ for (k in 1:5) {
 rmse
 mean(rmse)
 1 - (mean(rmse) / null)
+
+
+# Linear spatial Durbin error models
+well_nn <- knearneigh(well_ta, k = 5)
+wellnb <- knn2nb(well_nn)
+listw1 <- nb2listw(wellnb)
+
+reg.eq1 <- WellYield ~ TotalCompletedDepth + casing_length
+reg1 <- lmSLX(reg.eq1, data = well_ta@data, listw = listw1)
+summary(reg1)
+lm.LMtests(reg1, listw1, test = "all")
+impacts(reg1, listw = listw1)
+summary(impacts(reg1, listw = listw1, R = 500, zstats = TRUE))
+
+# Get point elevations and compute linear spatial Durbin error model
+elevations <- get_elev_point(well_ta, src = "aws")
+reg.eq2 <- WellYield ~ TotalCompletedDepth + casing_length + elevation
+reg2 <- lmSLX(reg.eq2, data = elevations@data, listw = listw1)
+lm.LMtests(reg2, listw1, test = "all")
+summary(reg2)
+
+# Just elevation in spatial error model
+reg.eq3 <- WellYield ~ elevation
+reg3 <- lmSLX(reg.eq3, data = elevations@data, listw = listw1)
+summary(reg3)
+
+#Get raster of elevations across the state
+locations <- well_ta@bbox
+locs <- data.frame(x = well_ta@bbox[1,], y = well_ta@bbox[2, ])
+locs$x <- well_ta@bbox[1,]
+locs$y <- well_ta@bbox[2]
+
+# DEM for california for predicting values including elevation 
+elev_rast <- get_elev_raster(locs, z = 1, prj = "+proj=aea +lat_1=32 +lat_2=42 +lat_0=0 +lon_0=-125 +x_0=0 +y_0=-4000000 +datum=NAD83 +units=m +ellps=GRS80 +towgs84=0,0,0")
+plot(elev_rast)
+
+# Random Forest model
+elev_rast <- raster(elevations)
+rf_coords <- as.data.frame(elevations@coords)
+rf_data <- elevations@data
+rf_data <- dplyr::select(rf_data, -DecimalLatitude, -DecimalLongitude, -elev_units)
+task = makeRegrTask(data = rf_data, target = "WellYield", coordinates = rf_coords)
+rf_learn = makeLearner(cl = "regr.ranger", predict.type = "response")
+
+# Tune random forest hyperparameters
+# spatial partitioning
+perf_level = makeResampleDesc("SpCV", iters = 5)
+# specifying random search
+ctrl = makeTuneControlRandom(maxit = 50L)
+
+ps = makeParamSet(
+  makeIntegerParam("mtry", lower = 1, upper = ncol(rf_data) - 1),
+  makeNumericParam("sample.fraction", lower = 0.2, upper = 0.9),
+  makeIntegerParam("min.node.size", lower = 1, upper = 10)
+)
+
+tune = tuneParams(learner = rf_learn, 
+                  task = task,
+                  resampling = perf_level,
+                  par.set = ps,
+                  control = ctrl, 
+                  measures = mlr::rmse)
+
+rf_learn = makeLearner(cl = "regr.ranger",
+                       predict.type = "response",
+                       mtry = tune$x$mtry, 
+                       sample.fraction = tune$x$sample.fraction,
+                       min.node.size = tune$x$min.node.size)
+rf_model = train(rf_learn, task)
+
+# Plot just Guernville
+local_map_data <- get_map(location = c(lon = -122.9769, lat = 38.5201), source = "google", maptype = "terrain", zoom = 12)
+ggmap(north_map_data)
+
+# Convert model prediction to polygons to map with ggmap
+final_raster <- projectRaster(idwr, crs = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+raster_poly <- rasterToPolygons(final_raster)
+raster_data <- as.data.frame(raster_poly)
+raster_data$id <- 1:nrow(raster_data)
+poly_plot <- polygons(raster_poly)
+poly_plot$pred <- raster_data
+fort <- fortify(poly_plot)
+fort_merge <- merge(fort, raster_data, by.x = "id", by.y = "id")
+
+# Specific location of interest -- extract prediction
+gville_house <- data.frame(lat = 38.503876, lon = -123.007919, elev = 284.988)
+gville_house <- SpatialPoints(matrix(c(-123.007919, 38.503876), nrow = 1, ncol = 2), proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs") )
+extract(final_raster, gville_house)
+
+# Plot local map
+raster_map <- ggmap(local_map_data)
+raster_map + 
+  geom_polygon(data = fort_merge, 
+               aes(x = long, y = lat, group = group, 
+                   fill = fort_merge$var1.pred), 
+               size = 0, 
+               alpha = 0.5) +
+  scale_fill_gradientn(colors = c('yellow', 'orange', 'blue', 'dark blue')) +
+  geom_point(data = north_wells, 
+             mapping = aes(y = DecimalLatitude, x = DecimalLongitude, color = production), cex = 2) +
+  geom_point(data = gville_house, mapping = aes(x = lon, y = lat), cex = 4)
